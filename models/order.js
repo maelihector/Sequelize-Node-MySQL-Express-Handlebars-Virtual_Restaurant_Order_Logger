ee390bb4f8647be070dc79052b67eef6a81147cb
mhector <maelihector@gmail.com>
2019-09-04 11:11:06 -0500

    Replace order methods that used the old ORM with a Sequelize model to create an Order table.

--- models/order.js
+++ models/order.js
@@ -1,23 +1,18 @@
-// Import the ORM to create functions that will interact with the database.
-const orm = require("../config/orm.js");
-
-const order = {
-  all: function(cb) {
-    orm.all("orders", function(res) {
-      cb(res);
-    });
-  },
-  create: function(cols, vals, cb) {
-    orm.create("orders", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("orders", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
+module.exports = function (sequelize, DataTypes) {
+  var Order = sequelize.define("Order", {
+    // Giving the Order model a name of type STRING
+    order_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    paid: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+  return Order;
 };
-
-// Export the database functions
-module.exports = order;
