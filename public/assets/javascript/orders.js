a60b81c815dabb99fe57198a692592e7d81cd308
mhector <maelihector@gmail.com>
2019-09-10 12:36:34 -0500

    Add function ‘toggleCustomerForm()’ to show customer either ‘new-customer-form’ or ‘existing-customer-form’ when entering a new order. Also add an ‘if/else’ statement to the ‘create-form’ submit event to check if the current customer already exists to avoid adding duplicate customers in database.

--- public/assets/javascript/orders.js
+++ public/assets/javascript/orders.js
@@ -1,27 +1,110 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function () {
-  // Add a new order with just new'order_name'
-  $(".create-form").on("submit", function (event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
+// Make sure the DOM is fully loaded
+$(document).ready(function () {
 
+  // Global variables
+  let customerExists;
+
+  // Function to create a new order
+  function sendOrder(customer, table) {
     let newOrder = {
       order_name: $("#new-order").val().trim(),
-    };
-
+      CustomerId: customer,
+      TableId: table
+    }
     // Send the POST request.
     $.ajax("/api/orders", {
       type: "POST",
       data: newOrder
-    }).then(
-      function () {
-        console.log("created new order");
-        // Reload the page to get the updated list
-        location.reload();
+    }).then(function () {
+      location.reload();
+    });
+  }
+
+  // Function to display correct customer form when entering a new order
+  function toggleCustomerForm() {
+    let existingCheckbox = document.querySelector('input[value="existing"]');
+    let newCheckbox = document.querySelector('input[value="new"]');
+    let newCustomerForm = $('.new-customer-form');
+    let existingCustomerForm = $('.existing-customer-form');
+    existingCustomerForm.css('display', 'none');
+    let tableSelectDiv = $("#table-select-div");
+
+    existingCheckbox.onchange = function () {
+      if (existingCheckbox.checked) {
+        existingCustomerForm.css('display', 'block');
+        newCustomerForm.css('display', 'none');
+        tableSelectDiv.css('display', 'none');
+        existingCheckbox.checked = false;
+        customerExists = true;
+      } else {
+        newCustomerForm.css('display', 'block');
       }
-    );
+    }
+
+    newCheckbox.onchange = function () {
+      if (newCheckbox.checked) {
+        newCustomerForm.css('display', 'block');
+        existingCustomerForm.css('display', 'none');
+        tableSelectDiv.css('display', 'block');
+        newCheckbox.checked = false;
+        customerExists = false;
+      } else {
+        existingCustomerForm.css('display', 'block');
+      }
+    }
+
+  }
+
+  // Call toggleCustomerForm on page load
+  toggleCustomerForm();
+
+  // Add a new customer
+  $(".create-form").on("submit", function (event) {
+    // Make sure to preventDefault on a submit event
+    event.preventDefault();
+
+    // If customer already exists grab their customer id and table number
+    if (customerExists) {
+
+      // Do not submit if customer didn't choose their name from the drop down
+      if ($("#existing-customer").find(':selected').attr('value') === '') {
+        return;
+      }
+      // Grab existing customer id and table number
+      let customerId = $("#existing-customer").find(':selected').attr('value');
+      let tableNum = $("#existing-customer").find(':selected').attr('table');
+
+      // Send new order with customer id and table number
+      sendOrder(customerId, tableNum);
+
+      // Else grab new customer id and table number
+    } else {
+
+      // Will not submit unless there is a name input
+      if (!($("#customer-name").val().trim())) {
+        return;
+      }
+
+      // Will not submit unless there is a table selected
+      if (!($("#table-select").val().trim())) {
+        return;
+      }
+
+      let newCustomer = {
+        name: $("#customer-name").val().trim(),
+        TableId: $("#table-select").val().trim()
+      };
+
+      $.ajax("/api/customers", {
+        type: "POST",
+        data: newCustomer
+
+        // After customer has been added with a table id, add their order
+      }).then(result => sendOrder(result.id, result.TableId));
+    }
   });
 
+  // Click event that changes the Order's devoured to true
   $(".change-devoured").on("click", function (event) {
     // Make sure to preventDefault on a click event.   
     event.preventDefault();
@@ -30,12 +113,11 @@ $(function () {
     let devoured = $(this).data("devoured");
 
     let newDevouredState = {
-      devoured: devoured,
-      paid: 0
+      devoured: devoured
     };
 
     // Send the PUT request.
-    $.ajax("/api/orders/" + id, {
+    $.ajax("/api/order-devoured/" + id, {
       type: "PUT",
       data: newDevouredState
     }).then(
@@ -47,6 +129,7 @@ $(function () {
     );
   });
 
+  // Click event that changes the Order's paid variable to true
   $(".change-paid").on("click", function (event) {
     // Make sure to preventDefault on a click event.   
     event.preventDefault();
@@ -54,15 +137,12 @@ $(function () {
     let id = $(this).data("id");
     let paid = $(this).data("paid");
 
-    console.log(paid);
-
     let newPaidState = {
-      devoured: 1,
       paid: paid
     };
 
     // Send the PUT request.
-    $.ajax("/api/orders/" + id, {
+    $.ajax("/api/order-paid/" + id, {
       type: "PUT",
       data: newPaidState
     }).then(
@@ -73,4 +153,5 @@ $(function () {
       }
     );
   });
+
 });
