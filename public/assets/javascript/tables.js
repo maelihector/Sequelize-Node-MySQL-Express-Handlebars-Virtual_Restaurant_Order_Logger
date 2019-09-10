3e47a00c9d53a63c0772c96167a6a5f8f3276abc
mhector <maelihector@gmail.com>
2019-09-10 12:37:33 -0500

    Separate table model javascript into own .js file.

---
+++ public/assets/javascript/tables.js
@@ -0,0 +1,12 @@
+// Make sure the DOM is fully loaded
+$(document).ready(function() {
+  
+  // Function needed to load landing page
+  function getTables() {
+    $.get("/api/tables");
+  }
+  
+  // Load Table data as soon as page loads
+  getTables();
+
+});
