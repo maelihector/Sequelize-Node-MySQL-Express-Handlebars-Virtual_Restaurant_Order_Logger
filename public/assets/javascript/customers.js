419f2688f66870ee7e95f4bc5fbcbbae78b64389
mhector <maelihector@gmail.com>
2019-09-12 15:06:04 -0500

    Delete some JS  and used handlebars helpers at index.handlebars to render current customer <option> elements. Add functions that append a delete btn to customer divs, and updates api when a customer has ‘leftTable’ (leftTable=true).

--- public/assets/javascript/customers.js
+++ public/assets/javascript/customers.js
@@ -1,64 +1,48 @@
 // Make sure the DOM is fully loaded
 $(document).ready(function () {
 
-  // Global variables
-  let customerSelect = $("#existing-customer");
-
   // Function to get list of all customers and render the list on the DOM
   function getCustomers() {
-    $.get("/api/customers", renderCustomerList);
+    $.get("/api/customers")
+      .then((data) => checkPaidCustomers(data))
+      .then(() => filterCustomers())
+      .then(() => addDeleteBtns())
   }
 
-  // Functions to build, and append an <option> for each customer in the database for the dropdown <select> list
-  function renderCustomerList(data) {
-    let rowsToAdd = [];
-    for (let i = 0; i < data.length; i++) {
-      rowsToAdd.push(createCustomerRow(data[i]));
-    }
-    customerSelect.empty();
-    customerSelect.append("<option value='' table=''>Select Customer</option>");
-    customerSelect.append(rowsToAdd);
-
-    // Call function that checks which customer are paid in full
-    checkPaidCustomers(data);
-  }
-  // Function that creates each row within the customer list
-  function createCustomerRow(customer) {
-    let listOption = $("<option>");
-    listOption.attr("value", customer.id);
-    listOption.attr("table", customer.TableId);
-    listOption.text(customer.name);
-    return listOption;
-  }
+  // Create variables to store paid and unpaid customers
+  const paidCustomers = [];
+  const unpaidCustomers = [];
 
   // Function that checks which customers are paid up
   function checkPaidCustomers(data) {
 
-    // Create variables to store paid and unpaid customers
-    let paidCustomers = [];
-    let unpaidCustomers = [];
+    for (let i = 0; i < data.length; i++) {
 
-    // Loop through each customer object in customers array
-    $.each(data, function (customers, customer) {
+      if (!data[i].leftTable) {
 
-      // Grab reference to each customer's Orders array, and customer id
-      let customerOrders = customer.Orders;
-      let customerId = customer.id;
+        // Grab reference to each customer's Orders array, and customer id
+        let customerOrders = data[i].Orders;
+        let customerId = data[i].id;
 
-      // Loop through each customer's Orders array
-      $.each(customerOrders, function (order, paid) {
+        // Loop through each customer's Orders array
+        $.each(customerOrders, function (order, paid) {
 
-        // Grab reference to each order's paid value
-        let paidUp = paid.paid;
-        // If order is paid=true, push customer id to paidCustomers array, else push to unpaidCustomers array
-        if (paidUp) {
-          paidCustomers.push(customerId);
-        } else {
-          unpaidCustomers.push(customerId);
-        }
+          // Grab reference to each order's paid value
+          let paidUp = paid.paid;
+          // If order is paid=true, push customer id to paidCustomers array, else push to unpaidCustomers array
+          if (paidUp) {
+            paidCustomers.push(customerId);
+          } else {
+            unpaidCustomers.push(customerId);
+          }
 
-      });
-    });
+        });
+      }
+    }
+  }
+
+
+  function filterCustomers() {
 
     // Filter duplicate customer ids from both arrays (duplicates are present because each customer can have several orders)
     let uniquePaidCustomers = paidCustomers.filter((item, i, array) => array.indexOf(item) === i);
@@ -66,26 +50,61 @@ $(document).ready(function () {
     // Get array of customers where  only ALL orders are paid for (not if customer also has an unpaid order)
     let allPaidCustomers = ($(uniquePaidCustomers).not(uniqueUnpaidCustomers).get());
 
-    // Only call the grayCustomerOut function if there is at least one allPaidCustomer present
+    // Add a delete btn to customers who are paid up
     if (allPaidCustomers.length > 0) {
-      grayCustomerOut(allPaidCustomers);
-    }
-  }
 
-  // Function that grays out customer with all orders paid
-  function grayCustomerOut(allPaidCustomers) {
+      // Loop through all paid customers
+      for (let i = 0; i < allPaidCustomers.length; i++) {
 
-    // Loop through all paid customers
-    for (let i = 0; i < allPaidCustomers.length; i++) {
-      // Find customer's name element on the DOM
-      let customerSeat = document.querySelector('h3[customerId="' + allPaidCustomers[i] + '"]');
-      // Gray out element
-      $(customerSeat).css("color", "#8c8989");
-
+        // Find customer's name element on the DOM
+        let customerSeat = document.querySelector('h3[customerId="' + allPaidCustomers[i] + '"]');
+        // Gray out element
+        $(customerSeat).css("color", "#8c8989");
+        // Create and add button
+        let btn = $("<button>");
+        btn.attr("id", "leftTable");
+        btn.text("X");
+        // Grab reference to parent element where button should append to
+        customerSeat = customerSeat.parentElement;
+        // Append button
+        $(customerSeat).append(btn);
+      }
     }
   }
 
   // Call function to render customers to DOM on page load
   getCustomers();
+
+
+
+
+  function addDeleteBtns() {
+
+    const buttons = document.querySelectorAll("#leftTable");
+
+    for (const button of buttons) {
+      button.addEventListener('click', updateDatabase);
+    }
+
+  }
+
+
+  function updateDatabase() {
+
+    console.log($(this));
+    console.log($(this)[0].parentElement.parentElement);
+
+    let customerToUpdate = $(this)[0].previousElementSibling.attributes[0].value;
+    console.log(customerToUpdate);
+    let divToRemove = $(this)[0].parentElement.parentElement;
+    $(divToRemove).remove();
+
+    $.ajax("/api/customers/" + customerToUpdate, {
+      type: "PUT"
+    }).then(() => location.reload())
+
+  }
+
+
 
 });
